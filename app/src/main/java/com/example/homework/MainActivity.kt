622a7a268b63760538d2a9488ded2e595cc95bdd
daniel-mvp <turdan243@gmail.com>
2022-10-09 15:00:50 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.homework.databinding.ActivityMainBinding
+import java.lang.Math
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initClicker()
+    }
+
+    private fun initClicker() {
+        binding.btnPlus.setOnClickListener {
+            binding.tvResult.text = Math().add(
+                binding.etFirstNumber.text.toString(),
+                binding.etSecondNumber.text.toString()
+            )
+        }
+        binding.btnDivide.setOnClickListener {
+            binding.tvResult.text = Math().divide(
+                binding.etFirstNumber.text.toString(),
+                binding.etSecondNumber.text.toString()
+            )
+        }
+    }
+}
